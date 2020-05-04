e49b81c63e8502a3973aeac8580eeb56e4d28e91
mogere <pmnyatichi@gmail.com>
2020-05-04 17:21:14 +0300

    add Uri builder,progressbar and scrollview

--- app/src/main/java/com/mogere/books/MainActivity.java
+++ app/src/main/java/com/mogere/books/MainActivity.java
@@ -5,17 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+    private ProgressBar mLoading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mLoading = (ProgressBar) findViewById(R.id.loading);
         try {
             URL bookUrl = ApiUtil.buildUrl("cooking");
             new BooksQueryTask().execute(bookUrl);
@@ -47,7 +51,23 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String result){
             TextView mResponse = (TextView) findViewById(R.id.response);
+            TextView mError = (TextView) findViewById(R.id.error);
+            mLoading.setVisibility(View.INVISIBLE);
+            if (result == null){
+                mResponse.setVisibility(View.INVISIBLE);
+                mError.setVisibility(View.VISIBLE);
+            }
+            else{
+                mResponse.setVisibility(View.VISIBLE);
+                mError.setVisibility(View.INVISIBLE);
+            }
             mResponse.setText(result);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mLoading.setVisibility(View.VISIBLE);
         }
     }
 }
